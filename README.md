4fc1e06052a99cad3d7d9d3e656bc0721199cf90
Akhyar Amarullah <akhyrul@gmail.com>
2015-03-02 15:35:39 +0700

    Add pull-request commenter

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# jenkins-bitbucket-pullrequest
+# Jenkins Bitbucket Pull Request
 Scripts to receive and update Bitbucket pull request for Jenkins integration.
+
+## Features
+
+*WIP*
+
+## Requirements
+
+* Build Token Root plugin
+
+## Usage
+
+*WIP*
+
---
+++ pull-request.php
@@ -0,0 +1,34 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$config_name = $_REQUEST['config'];
+$config = json_decode(file_get_contents("./configs/{$config_name}.json"));
+
+$pull_request_id = $_REQUEST['pull_request_id'];
+$status = $_REQUEST['status']; // pass | fail
+
+
+if ($pull_request_id === null || $pull_request_id == '') {
+  exit('Pull request ID required');
+} else {
+  $username = $config->bitbucket_username;
+  $password = $config->bitbucket_password;
+  $repo_owner = $config->bitbucket_repo_owner;
+  $repo_name = $config->bitbucket_repo_name;
+  $credential = new Bitbucket\API\Authentication\Basic($username, $password);
+
+  // TODO approve/decline pull request
+  $message = 'Hmm...'
+  if (strtolower($status) === 'pass') {
+    $message = 'Test passed';
+  } else {
+    $message = 'Test failed';
+  }
+
+  $pr = new Bitbucket\API\Repositories\PullRequests();
+  $pr->setCredentials($credential);
+  $result = $pr->comments()->create($repo_owner, $repo_name, $pull_request_id, $message);
+
+  var_dump($result);
+}
